6a736e773e527bd3a726050d679af26dde219a6a
Steve Korshakov <steve@korshakov.com>
2023-01-17 12:41:40 +0400

    feat: add getting started from scratch

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,5 +2,13 @@ export default {
     logo: <span>TACT Documentation</span>,
     project: {
         link: 'https://github.com/ton-community/tact',
-    }
+    },
+    feedback: {
+        content: null
+    },
+    footer: {
+        text: <span>
+          MIT {new Date().getFullYear()} © <a href="https://github.com/ton-community" target="_blank">TON Community</a>.
+        </span>,
+      }
 }
