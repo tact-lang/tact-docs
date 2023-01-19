a366ab02ecf6a170acbd72d8de3da47d0868499d
Steve Korshakov <steve@korshakov.com>
2023-01-19 14:24:22 +0400

    ref: update theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,20 +1,44 @@
 export default {
-    logo: <span>TACT Documentation</span>,
-    defaultTheme: 'dark',
-    project: {
-        link: 'https://github.com/ton-community/tact',
-    },
-    feedback: {
-        content: null
-    },
-    footer: {
-        text: <span>
-          MIT {new Date().getFullYear()} © <a href="https://github.com/ton-community" target="_blank">TON Community</a>.
-        </span>,
-      },
-      useNextSeoProps() {
-          return {
-            titleTemplate: '%s – TACT'
-          }
-      },
+  logo: <span>TACT Documentation</span>,
+  defaultTheme: 'dark',
+  project: {
+    link: 'https://github.com/ton-community/tact',
+  },
+  feedback: {
+    content: null
+  },
+  footer: {
+    text: <span>
+      MIT {new Date().getFullYear()} © <a href="https://github.com/ton-community" target="_blank">TON Community</a>.
+    </span>,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – TACT'
+    }
+  },
+  head: () => (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <meta
+        name="description"
+        content="TACT Language Documentation"
+      />
+      
+      <meta
+        name="og:title"
+        content="TACT Language Documentation"
+      />
+      <meta
+        name="og:description"
+        content="Language reference and guides for TACT"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ex3ndr"></meta>
+
+      <meta name="apple-mobile-web-app-title" content="TACT"/>
+    </>
+  )
 }
