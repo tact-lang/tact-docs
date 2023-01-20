1e89313ee7fda62e302127dbd7e7b22fb8d9beb2
Steve Korshakov <steve@korshakov.com>
2023-01-20 14:56:46 +0400

    wip: working on adding grammars

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
+const { getHighlighter, BUNDLED_LANGUAGES } = require('shiki');
+const fs = require('fs');
+const grammarTact = JSON.parse(fs.readFileSync(require.resolve('./grammar-tact.json'), 'utf-8'));
+const grammarFunc = JSON.parse(fs.readFileSync(require.resolve('./grammar-func.json'), 'utf-8'));
+
+const rehypePrettyCodeOptions = {
+  getHighlighter: options => {
+    return getHighlighter({
+      ...options,
+      langs: [
+        ...BUNDLED_LANGUAGES,
+        {
+          id: 'tact',
+          scopeName: 'source.tact',
+          grammar: grammarTact,
+          aliases: ['tact'],
+        },
+        {
+          id: 'func',
+          scopeName: 'source.func',
+          grammar: grammarFunc,
+          aliases: ['func'],
+        },
+      ],
+    });
+  },
+};
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
+  mdxOptions: {
+    rehypePrettyCodeOptions
+  }
 });
 
 module.exports = withNextra({
