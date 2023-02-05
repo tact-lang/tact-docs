b607a432ba403e841350896ac5a40696f6c2f9fe
Steve Korshakov <steve@korshakov.com>
2023-02-05 19:19:33 +0400

    feat: add vercel analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  };
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <Analytics />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
