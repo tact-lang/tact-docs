0a8398077129898c6f7a6ea929496cc280631b6f
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-02-29 22:50:19 +0100

    fix[CI]: Updated `scripts/proposals-generate.js`
    A follow-up PR for #69

--- scripts/proposals-generate.js
+++ scripts/proposals-generate.js
@@ -1,19 +1,21 @@
 const matter = require('gray-matter');
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
 // Update metadata
 let meta = {
     "index": "Evolution Process"
 };
-const files = fs.readdirSync(__dirname + '/pages/evolution');
+const cwd = process.cwd(); // current working directory of the process
+const files = fs.readdirSync(cwd + '/pages/evolution');
 for (let f of files) {
     if (f.endsWith('.mdx') && f.startsWith("OTP-")) {
         let name = f.substring(0, f.length - 4);
-        let title = matter(fs.readFileSync(path.join(__dirname, '/pages/evolution', f), 'utf8')).data.title;
+        let title = matter(fs.readFileSync(path.join(cwd, '/pages/evolution', f), 'utf8')).data.title;
         meta[name] = title;
     }
 }
 
 // Write metadata
-fs.writeFileSync(path.join(__dirname, '/pages/evolution/_meta.json'), JSON.stringify(meta, null, 4));
+fs.writeFileSync(path.join(cwd, '/pages/evolution/_meta.json'), JSON.stringify(meta, null, 4));
