3e26c8e6adc18eee051abde857c163e977c14971
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-04-03 19:49:22 +0200

    chore: Moved single-contract communication (mostly about sending stuff)

--- pages/cookbook/_meta.js
+++ pages/cookbook/_meta.js
@@ -1,12 +1,21 @@
 export default {
   index: 'Overview',
+  '-- 1': {
+    type: 'separator',
+    title: 'Single contract',
+  },
   'single-communication': 'Single-contract communication',
   'primitive-types': 'Primitive types',
   'data-structures': 'Data structures',
   algo: 'Algorithms',
+  time: 'Time and date',
   access: 'Access control',
   randomness: 'Randomness',
   misc: 'Miscellaneous',
+  '-- 2+': {
+    type: 'separator',
+    title: 'Multiple contracts',
+  },
   'mass-communication': 'Mass, multi-contract communication',
   jettons: 'Fungible Tokens (Jettons)',
   nfts: 'Non-Fungible Tokens (NFTs)',
