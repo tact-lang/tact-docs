a2417648d1f45da3c16f0fb89f30776ff4a72f27
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-04-08 16:30:06 +0200

    chore: Started the big move

---
+++ pages/ref/_meta.js
@@ -0,0 +1,41 @@
+export default {
+  index: 'Overview',
+  spec: 'Specification',
+  evolution: 'Evolution',
+  '-- API': {
+    type: 'separator',
+    title: 'API Reference',
+  },
+  'api-base': 'Base trait',
+  'api-comptime': 'Compile-time',
+  'api-common': 'Common', // what does it mean???
+  'api-strings': 'Strings', // text.tact + ...
+  'api-random': 'Random',
+  'api-math': 'Math',
+  'api-cells': 'Cells, Builders and Slices',
+  'api-advanced': 'Advanced',
+  '-- Stdlib': {
+    type: 'separator',
+    title: 'Standard Libraries',
+  },
+  'standard-libraries': 'Overview',
+  'stdlib-config': '@stdlib/config',
+  'stdlib-content': '@stdlib/content',
+  'stdlib-deploy': '@stdlib/deploy',
+  'stdlib-dns': '@stdlib/dns',
+  'stdlib-ownable': '@stdlib/ownable',
+  'stdlib-stoppable': '@stdlib/stoppable',
+  '-- Community': {
+    type: 'separator',
+  },
+  'telegram-link': {
+    title: '✈️ Telegram',
+    href: 'https://t.me/tactlang',
+    newWindow: true
+  },
+  'xtwitter-link': {
+    title: '🐦 X/Twitter',
+    href: 'https://twitter.com/tact_language',
+    newWindow: true
+  },
+}
